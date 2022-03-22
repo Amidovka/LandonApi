8baad3d2673e5128497045af777c519b88be5b70
Amidovka <amidovka@gmail.com>
2022-03-22 22:13:02 +0100

    First commit

---
+++ LandonApi/Controllers/RootController.cs
@@ -0,0 +1,45 @@
+﻿using LandonApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        [ProducesResponseType(200)]
+        public IActionResult GetRoot()
+        {
+            /*var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                rooms = new
+                {
+                    href = Url.Link(nameof(RoomsController.GetRooms), null)
+                },
+                info = new
+                {
+                    href = Url.Link(nameof(InfoController.GetInfo), null)
+                }
+            };*/
+
+            var response = new RootResponse
+            {
+                Self = Link.To(nameof(GetRoot)),
+                Rooms = Link.To(nameof(RoomsController.GetRooms)),
+                Info = Link.To(nameof(InfoController.GetInfo))
+                /*RoomsController = Url.Link(nameof(RoomsController.GetRooms), null),
+                Info = Url.Link(nameof(InfoController.GetInfo), null)*/
+            };
+
+            return Ok(response);
+        }
+    }
+}
+
